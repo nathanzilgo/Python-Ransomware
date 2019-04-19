0779943d0d040d64846eb453e35bb5c18877e2f4
nathanzilgo <nathanfpedroza@gmail.com>
2019-04-19 01:26:23 -0300

    ...

---
+++ Crypto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.6
+
+# utilizando criptografia de bloco assimétrica
+# Função de mudança de arquivo.
+def cryptAndDecryptFiles(filename, cryptoFn, blockSize=16):
+
+    # Abre o arquivo 'filename' a ser criptografado no modo de leitura 'r+b' binário
+    # Salva o arquivo aberto em _file
+    with open(filename, 'r+b') as _file:
+        # raw_value guarda uma leitura por blocos de 16 bits de _file.
+        raw_value = _file.read(blockSize)
+
+        # Aplica a função de criptografar a cada bloco
+        # de raw_value, cifrando o arquivo.
+        # Cada loop corresponde a um bloco de 16 bits.
+        while raw_value:
+
+            cipher_value = cryptoFn(raw_value)
+            # Compara o tamanho do bloco cifrado e o original
+            if len(raw_value) != len(cipher_value):
+                raise ValueError('Valor cifrado difere em tamanho {} do original {}'.format(len(cipher_value), len(raw_value)))
+
+            # Aponta para o inicio do arquivo original e o sobescreve com 'cipher_value'
+            _file.seek(- len(raw_value), 1)
+            _file.write(cipher_value)
+            raw_value = _file.read(blockSize)
